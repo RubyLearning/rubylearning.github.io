e4d0f24b667d1b8b1cca12f3ed2e2f0a9107f0b9
KOTP <keeperotphones@gmail.com>
2022-07-08 02:00:21 -0400

    Update site URL

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RubyLearning.com
+# rubylearning.github.io
 
 
 ### Running the app locally
--- js/msg.js
+++ js/msg.js
@@ -1,4 +1,4 @@
 function msg(){
-var msg= "&quot;A <strong><a href=\"http://rubylearning.com/blog/2007/01/20/setting-up-an-it-company-in-india/\" >step-by-step guide</a></strong> for foreign, small-sized software companies planning to setup their own IT companies in India.&quot;"
+var msg= "&quot;A <strong><a href=\"http://rubylearning.github.io/blog/2007/01/20/setting-up-an-it-company-in-india/\" >step-by-step guide</a></strong> for foreign, small-sized software companies planning to setup their own IT companies in India.&quot;"
 document.write(msg)
 }
